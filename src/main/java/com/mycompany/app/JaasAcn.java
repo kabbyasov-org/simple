f8c9f1c4b12169bcf96f0f713f3d11f961e57bb4
Konstantin Abbyasov <kabbyasov@serena.com>
2014-10-13 16:37:39 +0300

    sdasd

---
+++ src/main/java/com/mycompany/app/JaasAcn.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kabbyasov
+ * Date: 09.10.14
+ * Time: 16:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class JaasAcn {
+}
